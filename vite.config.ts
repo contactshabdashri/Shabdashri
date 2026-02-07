f66bbe7e2c0012783b31331c408bdc7f9c80c44d
brandbandhu <brandbandhu.praavi@gmail.com>
2026-02-07 17:44:58 +0530

    fix vite base + plugins

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
+  plugins: [react(),
+  mode === "development" && componentTagger(),]
+  .filter(Boolean),
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +16,6 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
